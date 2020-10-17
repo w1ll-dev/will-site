f9a6f35972dcad4a337531c3dd6a2f8fd22652b9
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-17 18:37:35 -0300

    add list items with routes enum

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Menu } from "../Menu";
 import { UserAvatar, Wrapper } from "./style";
-import homeIcon from '../../assets/icons/home.svg';
-import aboutIcon from '../../assets/icons/about.svg';
-import contactIcon from '../../assets/icons/contact.svg';
+import homeIcon from "../../assets/icons/home.svg";
+import aboutIcon from "../../assets/icons/about.svg";
+import contactIcon from "../../assets/icons/contact.svg";
 import { ToggleTheme } from "../ToggleTheme";
+import { RoutesENUM } from "../../contants";
 
 const items = [
-  { itemIcon: homeIcon, itemRoute: '/' },
-  { itemIcon: aboutIcon, itemRoute: '/' },
-  { itemIcon: contactIcon, itemRoute: '/' }
-]
+  { itemIcon: homeIcon, itemRoute: RoutesENUM.HOME },
+  { itemIcon: aboutIcon, itemRoute: RoutesENUM.ABOUT_ME },
+  { itemIcon: contactIcon, itemRoute: RoutesENUM.CONTACT },
+];
 
 export const Sidebar = () => {
   return (
     <Wrapper>
       <UserAvatar />
-      <Menu menuItemsList={items}/>
-      <ToggleTheme/>
+      <Menu menuItemsList={items} />
+      <ToggleTheme />
     </Wrapper>
   );
 };
