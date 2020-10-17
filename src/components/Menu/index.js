9e503a2b1893347dd20f029659a364a6286651dc
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-17 18:33:17 -0300

    add links to menu icons

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import { MenuList, MenuItem, ItemIcon } from "./style";
 
 export const Menu = ({ menuItemsList }) => {
+  const { pathname: currentRoute } = useLocation();
+
   return (
     <MenuList>
       {menuItemsList.map(({ itemIcon, itemRoute }, index) => (
         <MenuItem key={Math.random()}>
-          <ItemIcon src={itemIcon} />
+          <Link to={itemRoute}>
+            <ItemIcon src={itemIcon} active={itemRoute === currentRoute} />
+          </Link>
         </MenuItem>
       ))}
     </MenuList>
--- src/components/Menu/style.js
+++ src/components/Menu/style.js
@@ -11,10 +11,16 @@ export const MenuList = styled.ul`
 export const MenuItem = styled.li`
   cursor: pointer;
   display: flex;
+  padding: .5rem;
 `;
 
 export const ItemIcon = styled.img`
   width: 2rem;
   height: 2rem;
   opacity: ${({active}) => !active && 0.4};
+  transition: 500ms;
+  transform: ${({active}) => active && "scale(1.4)"};
+  &:hover{ 
+    transform: scale(1.4);
+  }
 `;
