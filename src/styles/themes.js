fa72f232d00833079d1d221f5d3b12e010cea510
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-17 16:00:38 -0300

    add initial theme setup

---
+++ src/styles/themes.js
@@ -0,0 +1,12 @@
+export default {
+  dark: {
+    backgroundSidebar: "#333",
+    backgroundContent: "#fff",
+    text: "#fff",
+  },
+  light: {
+    text: "#000",
+    backgroundSidebar: "#fff",
+    backgroundContent: "#333",
+  },
+};
