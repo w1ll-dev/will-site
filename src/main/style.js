9b5bdb108cba72f4f7310ca8134a3eb2bb33e489
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-17 16:10:15 -0300

    add Main component

---
+++ src/main/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Container } from "../components/commom";
+
+export const Main = styled(Container)`
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
