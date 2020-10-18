4b0170debae3731ca813148ce97c44e4bf4ed642
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 17:01:11 -0300

    fix text

--- src/assets/text/pt.js
+++ src/assets/text/pt.js
@@ -3,7 +3,7 @@ export const text = {
   about_me_title: "Um pouco sobre mim",
   my_name: "Will Márcio",
   short_summary: "Desenvolvedor de Software com ReactJS, React Native e Flutter.",
-  complete_summary: "Olá e bem-vindo, você está no site de uma pessoa que é apaixonada por desenvolvimento de software e que tomou a decisão de trabalhar com o que gosta há aproximadamente dois anos. Durante esse período desenvolvi alguns projetos pessoais e trabalhei para uma empresa do setor alimentício onde aprendi muito e fiz grandes amigos, você saberá um pouco mais sobre isso na aba de experiências.",
+  complete_summary: "Olá e bem-vindo, este é o curriculo de uma pessoa que é apaixonada por desenvolvimento de software e que tomou a decisão de trabalhar com o que gosta há aproximadamente dois anos. Durante esse período desenvolvi alguns projetos pessoais e atualmente trabalho para uma empresa do setor alimentício onde aprendi muito e fiz grandes amigos, você saberá um pouco mais sobre isso na aba de experiências.",
 
   skillsPageTitle: "Principais skills",
   skills: {
@@ -13,11 +13,29 @@ export const text = {
     },
     reactjs: {
       title: "React",
-      summary: "Com o framwork react construo aplicações responsivas seguindo padrões de projeto e que podem utilizar varios tipos de features como autenticação, atualização simultânea de dados para vários usuários, consumo de rest api, etc;"
+      summary: "Com o framwork react construo aplicações responsivas seguindo padrões de projeto e que podem utilizar vários tipos de features como autenticação, atualização simultânea de dados para vários usuários, consumo de rest api, etc;"
     },
     nodejs: {
       title: "Nodejs",
       summary: "Utilizando o nodejs sou capaz de criar rest apis que acessam, fazem alterações em bancos de dados e fornecem esses dados por meio de rotas http, tambem consigo adicionar funcionalidades como autenticação com jwt por exemplo;"
     }
+  },
+  lastJob: {
+    title: "Estagiário de Desenvolvimento na empresa Sangati Berga.",
+    resume: "Atualmente trabalho como estagiário de desenvolvimento na empresa Sangati Berga, nesse período tive ótimas experiências e conheci um pouco mais sobre como funciona e desenvolvimento de software em empresas do setor industrial. Durante o período tive três principais responsabilidades:",
+    jobResposabilities: [
+      {
+        title: "Controle de máquina.",
+        summary: "Desenvolvimento de software mobile para controle de Máquina nas linguagens dart, javascript e c++. Durante o período utilizei o framework flutter no frontend, no back-end utilizei nodejs e c++.",
+      },
+      {
+        title: "Tempo é dinheiro.",
+        summary: "Desenvolvimento de software para gerenciamento de prazos e tarefas do escritório técnico em linguagem Javascript utilizando react no frontend, e nodejs no Backend.",
+      },
+      {
+        title: "Revisão de código",
+        summary: "Auxilio nos códigos dart e javascript dos outros dois Desenvolvedores que trabalham comigo.",
+      },
+    ]
   }
 }
