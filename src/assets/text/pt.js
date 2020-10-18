895f090e799ab3c62534901d5da74010144a75fd
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 13:06:21 -0300

    add text to skills page

--- src/assets/text/pt.js
+++ src/assets/text/pt.js
@@ -1,7 +1,23 @@
-export default {
+export const text = {
   welcome: "Bem Vindo",
   about_me_title: "Um pouco sobre mim",
   my_name: "Will Márcio",
   short_summary: "Desenvolvedor de Software com ReactJS, React Native e Flutter.",
-  complete_summary: "Olá e bem-vindo, você está no site de uma pessoa que é apaixonada por desenvolvimento de software e que tomou a decisão de trabalhar com o que gosta há aproximadamente dois anos. Durante esse período desenvolvi alguns projetos pessoais e trabalhei para uma empresa do setor alimentício onde aprendi muito e fiz grandes amigos, você saberá um pouco mais sobre isso na aba de experiências."
+  complete_summary: "Olá e bem-vindo, você está no site de uma pessoa que é apaixonada por desenvolvimento de software e que tomou a decisão de trabalhar com o que gosta há aproximadamente dois anos. Durante esse período desenvolvi alguns projetos pessoais e trabalhei para uma empresa do setor alimentício onde aprendi muito e fiz grandes amigos, você saberá um pouco mais sobre isso na aba de experiências.",
+
+  skillsPageTitle: "Principais skills",
+  skills: {
+    flutter: {
+      title: "Flutter",
+      summary: "Com o framwork flutter do dart consigo contruir aplicações seguindo padrões de projeto e utillizando funcionalidades do smartphone como geolocalização, autenticação local com biometria e face id, consumindo rest apis do backend por meio de rotas http por exemplo;"
+    },
+    reactjs: {
+      title: "React",
+      summary: "Com o framwork react construo aplicações responsivas seguindo padrões de projeto e que podem utilizar varios tipos de features como autenticação, atualização simultânea de dados para vários usuários, consumo de rest api, etc;"
+    },
+    nodejs: {
+      title: "Nodejs",
+      summary: "Utilizando o nodejs sou capaz de criar rest apis que acessam, fazem alterações em bancos de dados e fornecem esses dados por meio de rotas http, tambem consigo adicionar funcionalidades como autenticação com jwt por exemplo;"
+    }
+  }
 }
