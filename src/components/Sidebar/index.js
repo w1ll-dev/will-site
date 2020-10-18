ee6a928b313fff8d22770c36b99038e17d66b840
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 13:06:56 -0300

    add code icon to skills page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import { Wrapper, MyAvatar } from "./style";
 import homeIcon from "../../assets/icons/home.svg";
 import aboutIcon from "../../assets/icons/about.svg";
 import contactIcon from "../../assets/icons/contact.svg";
+import codeIcon from "../../assets/icons/code.svg";
 import { ToggleTheme } from "../ToggleTheme";
 import { RoutesENUM } from "../../contants";
 
@@ -11,6 +12,7 @@ const items = [
   { itemIcon: homeIcon, itemRoute: RoutesENUM.HOME },
   { itemIcon: aboutIcon, itemRoute: RoutesENUM.ABOUT_ME },
   { itemIcon: contactIcon, itemRoute: RoutesENUM.CONTACT },
+  { itemIcon: codeIcon, itemRoute: RoutesENUM.CODE_SKILLS },
 ];
 
 export const Sidebar = () => {
