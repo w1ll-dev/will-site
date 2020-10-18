df205f0d146326791cdfe7a367a2875bbcdb5001
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 17:56:58 -0300

    update icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Menu } from "../Menu";
 import { Wrapper, MyAvatar } from "./style";
 import homeIcon from "../../assets/icons/home.svg";
-import aboutIcon from "../../assets/icons/about.svg";
+import experienceIcon from "../../assets/icons/experience.svg";
 import contactIcon from "../../assets/icons/contact.svg";
 import codeIcon from "../../assets/icons/code.svg";
 import { ToggleTheme } from "../ToggleTheme";
@@ -10,7 +10,7 @@ import { RoutesENUM } from "../../contants";
 
 const items = [
   { itemIcon: homeIcon, itemRoute: RoutesENUM.HOME },
-  { itemIcon: aboutIcon, itemRoute: RoutesENUM.ABOUT_ME },
+  { itemIcon: experienceIcon, itemRoute: RoutesENUM.EXPERIENCE },
   { itemIcon: contactIcon, itemRoute: RoutesENUM.CONTACT },
   { itemIcon: codeIcon, itemRoute: RoutesENUM.CODE_SKILLS },
 ];
