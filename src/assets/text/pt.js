14624e7d1ecf4cc4fc58c0af3d2d7d9d8b21eb24
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 17:56:25 -0300

    replace about icon for experience icon

--- src/assets/text/pt.js
+++ src/assets/text/pt.js
@@ -5,6 +5,7 @@ export const text = {
   short_summary: "Desenvolvedor de Software com ReactJS, React Native e Flutter.",
   complete_summary: "Olá e bem-vindo, este é o curriculo de uma pessoa que é apaixonada por desenvolvimento de software e que tomou a decisão de trabalhar com o que gosta há aproximadamente dois anos. Durante esse período desenvolvi alguns projetos pessoais e atualmente trabalho para uma empresa do setor alimentício onde aprendi muito e fiz grandes amigos, você saberá um pouco mais sobre isso na aba de experiências.",
 
+  experiencesPageTitle: "Experiências", 
   skillsPageTitle: "Principais skills",
   skills: {
     flutter: {
