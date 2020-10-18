ceb49dd48b50bbf14a79b653aac6cbef4c260b48
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 08:09:40 -0300

    clear code

--- src/components/Menu/style.js
+++ src/components/Menu/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import themes from "../../styles/themes";
 
 export const MenuList = styled.ul`
   display: flex;
@@ -11,16 +12,17 @@ export const MenuList = styled.ul`
 export const MenuItem = styled.li`
   cursor: pointer;
   display: flex;
-  padding: .5rem;
+  padding: 0.5rem;
 `;
 
 export const ItemIcon = styled.img`
   width: 2rem;
   height: 2rem;
-  opacity: ${({active}) => !active && 0.4};
+  opacity: ${({ active }) => !active && 0.4};
   transition: 500ms;
-  transform: ${({active}) => active && "scale(1.4)"};
-  &:hover{ 
+  transform: ${({ active }) => active && "scale(1.4)"};
+
+  &:hover {
     transform: scale(1.4);
   }
 `;
