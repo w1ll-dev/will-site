13e4e03bd30b3679a9d806581ed6b17361a5a1be
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 10:50:34 -0300

    add fruit icon and change complete summary text

--- src/assets/text/pt.js
+++ src/assets/text/pt.js
@@ -3,5 +3,5 @@ export default {
   about_me_title: "Um pouco sobre mim",
   my_name: "Will Márcio",
   short_summary: "Desenvolvedor de Software com ReactJS, React Native e Flutter.",
-  complete_summary: "Olá e bem-vindo, você está no site de uma pessoa que é apaixonada por desenvolvimento de software e que tomou a decisão de trabalhar com o que gosta há aproximadamente dois anos. Durante esse período desenvolvi alguns projetos pessoais e trabalhei para uma empresa do setor alimentício onde aprendi muito e fiz grandes amigos você saberá um pouco mais sobre isso na aba de experiências."
+  complete_summary: "Olá e bem-vindo, você está no site de uma pessoa que é apaixonada por desenvolvimento de software e que tomou a decisão de trabalhar com o que gosta há aproximadamente dois anos. Durante esse período desenvolvi alguns projetos pessoais e trabalhei para uma empresa do setor alimentício onde aprendi muito e fiz grandes amigos, você saberá um pouco mais sobre isso na aba de experiências."
 }
