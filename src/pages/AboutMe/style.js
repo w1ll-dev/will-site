6fd2ec63268e170eda17f7cad1307f865d76b613
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-18 16:59:56 -0300

    remove aboutme page

--- src/pages/AboutMe/style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { Container } from "../../components/commom/index";
-
-export const Wrapper = styled(Container)`
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-`;
