4ae597ff3a114b1799e49f97b9b12d6b1a0549f2
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-20 19:17:21 -0300

    destruct variables from text and add contactLabel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,6 +12,7 @@ import {
   ContactContainer,
   ContactTitle,
   ContactRow,
+  ContactLabel,
   ContactIcon,
 } from "./style";
 import githubIcon from "../../assets/icons/github.svg";
@@ -19,32 +20,45 @@ import githubWhiteIcon from "../../assets/images/github-white.png";
 import linkedinIcon from "../../assets/icons/linkedin.svg";
 import { text } from "../../assets/text/pt";
 import { LinkContainer } from "../../components/commom";
-import { Contacts} from "../../contants";
+import { Contacts } from "../../contants";
 import { useTheme } from "../../context/theme";
 
-const {LINKEDIN_URL, GITHUB_URL} = Contacts
+const { LINKEDIN_URL, GITHUB_URL } = Contacts;
+
+const {
+  contacts,
+  welcome,
+  my_name,
+  short_summary,
+  about_me_title,
+  complete_summary,
+} = text;
 
 export const HomePage = () => {
   const isMobile = window.innerWidth < 1200;
   const { activeTheme } = useTheme();
   return (
     <Wrapper>
-      <Welcome children={text.welcome} />
+      <Welcome children={welcome} />
+      <HomeAvatar />
       <PageContent>
-        <HomeAvatar />
-        <MyName children={text.my_name} />
-        <ShortSummary children={text.short_summary} />
-        <AboutMeTitle children={text.about_me_title} />
-        <CompleteSummary children={text.complete_summary} />
+        <MyName children={my_name} />
+        <ShortSummary children={short_summary} />
+        <AboutMeTitle children={about_me_title} />
+        <CompleteSummary children={complete_summary} />
         {isMobile && (
           <ContactContainer>
             <ContactTitle>Perfis</ContactTitle>
             <ContactRow>
               <LinkContainer href={LINKEDIN_URL}>
                 <ContactIcon src={linkedinIcon} />
+                <ContactLabel children={contacts.linkedIn} />
               </LinkContainer>
               <LinkContainer href={GITHUB_URL}>
-                <ContactIcon src={activeTheme === "light" ? githubIcon : githubWhiteIcon} />
+                <ContactIcon
+                  src={activeTheme === "light" ? githubIcon : githubWhiteIcon}
+                />
+                <ContactLabel children={contacts.github} />
               </LinkContainer>
             </ContactRow>
           </ContactContainer>
