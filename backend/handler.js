1ea0c688d59a176f645f0f938c540a7c4c84e8b9
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-03-25 07:01:09 -0300

    remove backend

--- backend/handler.js
+++
@@ -1,7 +0,0 @@
-const awsServerlessExpr = require("aws-serverless-express");
-const app = require("./src");
-
-const server = awsServerlessExpr.createServer(app);
-
-exports.handler = (event, context) =>
-  awsServerlessExpr.proxy(server, event, context);
