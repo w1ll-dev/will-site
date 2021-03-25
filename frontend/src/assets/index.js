8396397081bc332b16727494a2814646762495cb
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-03-25 07:12:49 -0300

    create file to export icons

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,45 @@
+import homeIcon from "./icons/home.svg";
+import experienceIcon from "./icons/experience.svg";
+import codeIcon from "./icons/code.svg";
+import githubIcon from "./icons/github.svg";
+import githubWhiteIcon from "./images/github-white.png";
+import linkedinIcon from "./icons/linkedin.svg";
+import brazilIcon from "./icons/brazil.svg";
+import usaIcon from "./icons/united-states.svg";
+import settingsIcon from "./icons/settings.svg";
+import sunIcon from "./icons/sun.svg";
+import moonIcon from "./icons/moon.svg";
+import codeReviewDarkIcon from "./icons/color-icons/dark/code-review.svg";
+import codeReviewLightIcon from "./icons/color-icons/light/code-review.svg";
+import flutterDevLightIcon from "./icons/color-icons/light/flutter-dev.svg";
+import flutterDevDarkIcon from "./icons/color-icons/dark/flutter-dev.svg";
+import reactDevLightIcon from "./icons/color-icons/light/react-dev.svg";
+import reactDevDarkIcon from "./icons/color-icons/dark/react-dev.svg";
+import maintenceLightIcon from "./icons/color-icons/light/maintence-light.svg";
+import maintenceDarkIcon from "./icons/color-icons/dark/maintence-dark.svg";
+import kidsLightIcon from "./icons/color-icons/light/kids-light.svg";
+import kidsDarkIcon from "./icons/color-icons/dark/kids-dark.svg";
+
+export {
+  homeIcon,
+  experienceIcon,
+  codeIcon,
+  githubIcon,
+  githubWhiteIcon,
+  linkedinIcon,
+  brazilIcon,
+  usaIcon,
+  settingsIcon,
+  sunIcon,
+  moonIcon,
+  codeReviewDarkIcon,
+  codeReviewLightIcon,
+  flutterDevLightIcon,
+  flutterDevDarkIcon,
+  reactDevLightIcon,
+  reactDevDarkIcon,
+  maintenceLightIcon,
+  maintenceDarkIcon,
+  kidsLightIcon,
+  kidsDarkIcon
+}
