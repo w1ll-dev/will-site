c0bc1533c5358953e47bf29d6b0844172b6786f2
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-03-25 08:19:19 -0300

    create icons list according experiences

---
+++ frontend/src/pages/Experiences/icons.js
@@ -0,0 +1,39 @@
+import {
+  codeReviewLightIcon,
+  flutterDevLightIcon,
+  reactDevLightIcon,
+  codeReviewDarkIcon,
+  flutterDevDarkIcon,
+  reactDevDarkIcon,
+  maintenceLightIcon,
+  maintenceDarkIcon,
+  kidsLightIcon,
+  kidsDarkIcon
+} from '../../assets'
+
+export const jobsIcons = [
+  [
+    {
+      light: flutterDevLightIcon,
+      dark: flutterDevDarkIcon
+    },
+    {
+      light: reactDevLightIcon,
+      dark: reactDevDarkIcon
+    },
+    {
+      light: codeReviewLightIcon,
+      dark: codeReviewDarkIcon
+    }
+  ],
+  [
+    {
+      light: kidsLightIcon,
+      dark:   kidsDarkIcon
+    },
+    {
+      light: maintenceLightIcon,
+      dark: maintenceDarkIcon
+    }
+  ]
+]
