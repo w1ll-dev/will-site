af750db89391569727cd67b3bc852b91b1da333c
willgomes-sl <will.gomes@spartalabs.com.br>
2023-01-04 10:59:51 -0300

    Updates

---
+++ precache-manifest.918d28768d36848431db4a97acc163ae.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9c16eb674da37c83341c7e9f00247d",
+    "url": "/will-site/index.html"
+  },
+  {
+    "revision": "91cb59e05ee1ecc53853",
+    "url": "/will-site/static/js/2.3562adfc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/will-site/static/js/2.3562adfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06172f7abedd288005d0",
+    "url": "/will-site/static/js/main.b5356863.chunk.js"
+  },
+  {
+    "revision": "b721ec71042f3a1050d9",
+    "url": "/will-site/static/js/runtime-main.633d3685.js"
+  },
+  {
+    "revision": "594fd14f77a6be6250cf5f2d26ba1f3a",
+    "url": "/will-site/static/media/brazil.594fd14f.svg"
+  },
+  {
+    "revision": "bfde4a5ac9a34920a587cb1b29c0230a",
+    "url": "/will-site/static/media/code-review.bfde4a5a.svg"
+  },
+  {
+    "revision": "f7e655ee49fc8f59680e1d583719de58",
+    "url": "/will-site/static/media/code-review.f7e655ee.svg"
+  },
+  {
+    "revision": "cca45114ba15a6365b2202d0ab1e2f1a",
+    "url": "/will-site/static/media/code.cca45114.svg"
+  },
+  {
+    "revision": "fb16be4ae044b13191dea8c7ff82647a",
+    "url": "/will-site/static/media/experience.fb16be4a.svg"
+  },
+  {
+    "revision": "3e33b05ce53c087e794e8a042fbeecae",
+    "url": "/will-site/static/media/flutter-dev.3e33b05c.svg"
+  },
+  {
+    "revision": "658bf582dad6078ecae444379ea8ee13",
+    "url": "/will-site/static/media/flutter-dev.658bf582.svg"
+  },
+  {
+    "revision": "303729d0930c9d5f80ac222a9d27908d",
+    "url": "/will-site/static/media/flutter.303729d0.png"
+  },
+  {
+    "revision": "0c768863b084475e3760cf618d154919",
+    "url": "/will-site/static/media/github-white.0c768863.png"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/will-site/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "77e2fcb801eefbe28403ba61a9382d02",
+    "url": "/will-site/static/media/home.77e2fcb8.svg"
+  },
+  {
+    "revision": "9164ee22dc883521580f0d684adc4f38",
+    "url": "/will-site/static/media/kids-dark.9164ee22.svg"
+  },
+  {
+    "revision": "c3cec8e7ba7276e8f3ac85847af3e40b",
+    "url": "/will-site/static/media/kids-light.c3cec8e7.svg"
+  },
+  {
+    "revision": "f54e1d688e321addb303f4d2b0c6709a",
+    "url": "/will-site/static/media/linkedin.f54e1d68.svg"
+  },
+  {
+    "revision": "40078e2825396e185ebff942fd524d9e",
+    "url": "/will-site/static/media/maintence-dark.40078e28.svg"
+  },
+  {
+    "revision": "65e2d61db232d609154225df85707ac8",
+    "url": "/will-site/static/media/maintence-light.65e2d61d.svg"
+  },
+  {
+    "revision": "b350f2e06807d6a1558f3e30c1dbca66",
+    "url": "/will-site/static/media/me.b350f2e0.jpeg"
+  },
+  {
+    "revision": "5039864cceefa1c9cf8f8a3aebbbd405",
+    "url": "/will-site/static/media/moon.5039864c.svg"
+  },
+  {
+    "revision": "9c4ab2032a71589bb23347e9f9f0564a",
+    "url": "/will-site/static/media/node.9c4ab203.png"
+  },
+  {
+    "revision": "7a7456c1603529775a2a2ae818964861",
+    "url": "/will-site/static/media/react-dev.7a7456c1.svg"
+  },
+  {
+    "revision": "85b6f6784a2fb2e0a9f8472b4d76ad40",
+    "url": "/will-site/static/media/react-dev.85b6f678.svg"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/will-site/static/media/react.04557c1b.png"
+  },
+  {
+    "revision": "9b3341c7999f1b5f38da139edd001ee1",
+    "url": "/will-site/static/media/settings.9b3341c7.svg"
+  },
+  {
+    "revision": "c5f0249e279fda399bfef8633497efba",
+    "url": "/will-site/static/media/sun.c5f0249e.svg"
+  },
+  {
+    "revision": "4a90ee18671e922d4eb77cd821ed821b",
+    "url": "/will-site/static/media/united-states.4a90ee18.svg"
+  }
+]);
